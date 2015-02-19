19c9c1c75f85672768eff98670a4781ef4776251
drh464 <drh464@users.noreply.github.com>
2015-02-19 15:47:12 -0600

    Update app_1.js

--- js/app_1.js
+++ js/app_1.js
@@ -37,6 +37,6 @@ featureLayer.on('ready', function(){
 
 featureLayer.on('ready', function(){
    this.eachLayer(function(layer){
-       layer.bindPopup('Name: ' + layer.feature.properties.name);
+       layer.bindPopup('Name: ' + layer.feature.properties.Name);
    }); 
 });
